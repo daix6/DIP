07015b5663c9410c2e535d3cd7c0eaccee8abff8
Shawn Dai <daixuan1996@gmail.com>
2015-12-19 00:43:01 +0800

    Gauss

--- hw4/src/main.py
+++ hw4/src/main.py
@@ -8,14 +8,21 @@ from filter2d import *
 def main():
   # Arguments
   parser = argparse.ArgumentParser()
-  parser.add_argument('-s', type=str, default='task_1.png', help='The path of the image that you want to deal with.')
+  parser.add_argument('-t', type=int, choices=[1,2,3], help='The task you want to run: 1, 2 or 3.')
+  parser.add_argument('-s', type=str, default='43.png', help='The path of the image that you want to deal with.')
   args = parser.parse_args()
 
   # Path
   file_dir = os.path.dirname(os.path.realpath(__file__))
   parent_dir = os.path.split(file_dir)[0]
   dist_dir = os.path.join(parent_dir, 'dist')
-  filename = os.path.join(parent_dir, 'assets', args.s)
+
+  if args.t == 1:
+    filename = os.path.join(parent_dir, 'assets', 'task_1.png')
+  elif args.t == 2:
+    filename = os.path.join(parent_dir, 'assets', 'task_2.png')
+  else:
+    filename = os.path.join(parent_dir, 'assets', 'task_3', args.s)
 
   if not os.path.exists(filename):
     raise Exception("There is no file named " + filename + ".")
@@ -34,26 +41,31 @@ def main():
 
     Image.fromarray(data).convert('L').save(os.path.join(dist, name))
 
-  # 2-1 filter
-  task_1 = open_image(filename)
-  task_1_arithmetic_mean_3x3 = arithmetic_mean_filter(task_1, (3,3))
-  save_image(task_1_arithmetic_mean_3x3, 'arithmetic_mean_3x3.png')
+  if args.t == 1:
+    # 2-1 filter
+    task_1 = open_image(filename)
+    task_1_arithmetic_mean_3x3 = arithmetic_mean_filter(task_1, (3,3))
+    save_image(task_1_arithmetic_mean_3x3, 'arithmetic_mean_3x3.png')
 
-  task_1_arithmetic_mean_9x9 = arithmetic_mean_filter(task_1, (9,9))
-  save_image(task_1_arithmetic_mean_9x9, 'arithmetic_mean_9x9.png')
+    task_1_arithmetic_mean_9x9 = arithmetic_mean_filter(task_1, (9,9))
+    save_image(task_1_arithmetic_mean_9x9, 'arithmetic_mean_9x9.png')
 
-  task_1_harmonic_mean_3x3 = harmonic_mean_filter(task_1, (3,3))
-  save_image(task_1_harmonic_mean_3x3, 'task_1_harmonic_mean_3x3.png')
+    task_1_harmonic_mean_3x3 = harmonic_mean_filter(task_1, (3,3))
+    save_image(task_1_harmonic_mean_3x3, 'task_1_harmonic_mean_3x3.png')
 
-  task_1_harmonic_mean_9x9 = harmonic_mean_filter(task_1, (9,9))
-  save_image(task_1_harmonic_mean_9x9, 'task_1_harmonic_mean_9x9.png')
+    task_1_harmonic_mean_9x9 = harmonic_mean_filter(task_1, (9,9))
+    save_image(task_1_harmonic_mean_9x9, 'task_1_harmonic_mean_9x9.png')
 
-  task_1_contra_harmonic_mean_3x3 = contra_harmonic_mean_filter(task_1, (3,3), -1.5)
-  save_image(task_1_contra_harmonic_mean_3x3, 'task_1_contra_harmonic_mean_3x3.png')
+    task_1_contra_harmonic_mean_3x3 = contra_harmonic_mean_filter(task_1, (3,3), -1.5)
+    save_image(task_1_contra_harmonic_mean_3x3, 'task_1_contra_harmonic_mean_3x3.png')
 
-  task_1_contra_harmonic_mean_9x9 = contra_harmonic_mean_filter(task_1, (9,9), -1.5)
-  save_image(task_1_contra_harmonic_mean_9x9, 'task_1_contra_harmonic_mean_9x9.png')
+    task_1_contra_harmonic_mean_9x9 = contra_harmonic_mean_filter(task_1, (9,9), -1.5)
+    save_image(task_1_contra_harmonic_mean_9x9, 'task_1_contra_harmonic_mean_9x9.png')
 
+  elif args.t == 2:
+    task_2 = open_image(filename)
+    task_2_gauss = add_guassian(task_2, 0.0, 40.0)
+    save_image(task_2_gauss, 'task_2_guass_0_40.png')
 
 if __name__ == '__main__':
   main()
---
+++ hw4/src/noise.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Ref: https://en.wikipedia.org/wiki/Normal_distribution#Generating_values_from_normal_distribution
+#      http://www.design.caltech.edu/erik/Misc/Gaussian.html
+#      https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+
+class GaussianGenerator(object):
+  def __init__(self, mu, sigma):
+    self.generate = True
+    self.mu = mu
+    self.sigma = sigma
+    self.cache = None
+
+  def generator(self):
+    if (!self.generate)
+      self.generate = not self.generate
+      return self.cache * self.sigma + self.mu
+
+    self.generate = not self.generate
+
+    u = np.random.random_sample()
+    v = np.random.random_sample()
+
+    r = np.sqrt(-2 * np.log(v))
+    theta = 2 * np.pi * u
+
+    z = r * np.cos(theta)
+    self.cache = r * np.sin(theta)
+
+    return z * self.sigma + self.mu
+
+  def generator_polar(self):
+    if (!self.generate)
+      self.generate = not self.generate
+      return self.cache * self.sigma + self.mu
+
+    self.generate = not self.generate
+
+    # between (-1,1)
+    u = 2 * np.random.random_sample() - 1
+    v = 2 * np.random.random_sample() - 1
+
+    s = u*u + v*v
+    r = sqrt((-2 * np.log(s)) / s)
+
+    z = u * r
+    self.cache = v * r
+
+    return z
+
+def add_gaussian(data, mu, sigma):
+  generator = GaussianGenerator(mu, sigma)
+
+  def add_noise(p):
+    return p + generator.generator()
+
+  vfunc = np.vectorize(add_noise)
+  return vfunc(data)
